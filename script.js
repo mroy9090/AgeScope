6795365a154889257cbcd80d6d9230ac1c03c3e6
Mridul Roy <55798279+mroy9090@users.noreply.github.com>
2025-07-18 19:41:09 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
-function checkAge(){
-  let ageInput = document.getElementById('ageInput').value;
+document.addEventListener('DOMContentLoaded' , ()=>{
+  let ageInput= document.getElementById('ageInput');
   let resultDiv = document.getElementById('ageResult');
-  if(isNaN(ageInput)){
-    resultDiv.innerHTML = `<div class="alert alert-warning">Please enter a valid age.</div>`;
-    return;
-  }
-  let cardContent= '';
-  if (ageInput>=18){
-    resultDiv.innerHTML = `
-      <div class="card border-success">
-        <div class="card-body text-success">
-          <h5 class="card-title">You are an adult</h5>
-          <p class="card-text">You are 18 or older.</p>
-        </div>
-      </div>`;
-  }
-  else{
-    resultDiv.innerHTML = `
-      <div class="card border-danger">
-        <div class="card-body text-danger">
-          <h5 class="card-title">You are an child</h5>
-          <p class="card-text">You are under 18 years old.</p>
-        </div>
-      </div>`;
-  }
-}
+  let cardBody = document.getElementById('cardBody');
+
+  ageInput.addEventListener('input', ()=>{
+    let age = ageInput.value;
+
+    if(age === '' || isNaN(age)){
+      resultDiv.innerHTML = '<div class="alert alert-warning">Please enter a valid age.</div>';
+
+    }
+    else if(age<18){
+      cardBody.innerHTML = `
+      <div class="col-md-4">
+          <div class="card h-100">
+              <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Child</h5>
+                  <p class="card-text">If your age is between 0 to 18 .</p>
+              </div>
+          </div>
+      </div>`
+    }
+    else if(age>18){
+      cardBody.innerHTML = `
+      <div class="col-md-4">
+          <div class="card h-100">
+              <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Adult</h5>
+                  <p class="card-text">If your age is between 0 to 18 .</p>
+              </div>
+          </div>
+      </div>`
+    }
+    else {
+      cardBody.innerHTML = `
+      <div class="col-md-4">
+          <div class="card h-100">
+              <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Adult</h5>
+                  <p class="card-text">If your age is between 0 to 18 .</p>
+              </div>
+          </div>
+      </div>
+      <div class="col-md-4">
+          <div class="card h-100">
+              <img src="https://via.placeholder.com/300x200" class="card-img-top" alt="...">
+              <div class="card-body">
+                  <h5 class="card-title">Adult</h5>
+                  <p class="card-text">If your age is between 0 to 18 .</p>
+              </div>
+          </div>
+      </div>`
+    }
+  })
+})
